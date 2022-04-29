3f4da1e620ffc912e06fe65009b1e3d36ffa2749
João Victor Cavalcante <101560833+jotacoding@users.noreply.github.com>
2022-04-29 10:33:12 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -22,7 +22,7 @@ console.log(text)
     }else{
       b.innerText = 'Show Info';
       text.style.display = 'none'
-      card.style.height = "430px";
+      card.style.height = "480px";
     }
 
 
